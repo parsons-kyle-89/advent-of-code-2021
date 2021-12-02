f1b71f6762532166ea4ee171f1243a0882eb4758
Kyle Parsons <parsons.kyle.89@gmail.com>
2021-12-02 00:09:25 -0500

    quick solution

--- day02/main.py
+++ day02/main.py
@@ -5,12 +5,39 @@ SCRIPT_DIR = os.path.dirname(os.path.relpath(__file__))
 
 def main() -> None:
     with open(f'{SCRIPT_DIR}/input.txt', 'r') as f:
-        answer_1, answer_2 = f.readlines()
+        moves = f.readlines()
 
-    assert answer_1 == 'hello\n'
+    horiz, depth = 0, 0
+    for move in moves:
+        direction, distance = move.split(maxsplit=1)
+        distance = int(distance)
+        match direction:
+            case 'forward':
+                horiz += distance
+            case 'up':
+                depth -= distance
+            case 'down':
+                depth += distance
+
+    answer_1 = horiz * depth
+    assert answer_1 == 1690020
     print(answer_1)
 
-    assert answer_2 == 'there\n'
+    horiz, depth, aim = 0, 0, 0
+    for move in moves:
+        direction, distance = move.split(maxsplit=1)
+        distance = int(distance)
+        match direction:
+            case 'forward':
+                horiz += distance
+                depth += distance * aim
+            case 'up':
+                aim -= distance
+            case 'down':
+                aim += distance
+
+    answer_2 = horiz * depth
+    assert answer_2 == 1408487760
     print(answer_2)
 
 
