9b16b64c788c4c19003cb4ee4d58064a2a4b11e0
Kyle Parsons <parsons.kyle.89@gmail.com>
2021-12-15 10:09:01 -0500

    cleanup more

--- day15/main.py
+++ day15/main.py
@@ -1,13 +1,19 @@
+from collections import defaultdict
 from itertools import product
 import os.path
+from typing import cast
 
 import numpy as np
 import numpy.typing as np_typing
 
+
 SCRIPT_DIR = os.path.dirname(os.path.relpath(__file__))
 
+Grid = np_typing.NDArray[np.int64]
+Point = tuple[int, int]
 
-def parse_risk(raw_risk: str) -> np_typing.NDArray[np.int64]:
+
+def parse_risk(raw_risk: str) -> Grid:
     return np.array([parse_row(raw_row) for raw_row in raw_risk.splitlines()])
 
 
@@ -15,13 +21,9 @@ def parse_row(raw_row: str) -> list[int]:
     return [int(raw_cell) for raw_cell in raw_row.strip()]
 
 
-def neighbors(
-    point: tuple[int, int],
-    max_x: int,
-    max_y: int,
-) -> list[tuple[int, int]]:
+def neighbors(point: Point, max_x: int, max_y: int) -> set[Point]:
     point_x, point_y = point
-    return [
+    return {
         (x, y)
         for x, y in (
             (point_x - 1, point_y), (point_x, point_y - 1),
@@ -29,40 +31,33 @@ def neighbors(
         )
         if 0 <= x < max_x
         if 0 <= y < max_y
-    ]
+    }
 
 
-def dijkstra(
-    risk: np_typing.NDArray[np.int64],
-    start: tuple[int, int],
-    end: tuple[int, int],
-) -> int:
+def dijkstra(risk: Grid, start: Point, end: Point) -> int:
     width, height = risk.shape
-    nodes = list(product(range(width), range(height)))
-    unvisited = set(nodes)
-    tentative_distances = {node: float('inf') for node in nodes}
+    tentative_distances: defaultdict[Point, int | float] = (
+        defaultdict(lambda: float('inf'))
+    )
     tentative_distances[start] = 0
+    visited = set()
     possible_current = {start}
 
-    while end in unvisited:
+    while end not in visited:
         current = min(possible_current, key=tentative_distances.__getitem__)
         possible_current.remove(current)
-        for neighbor in neighbors(current, width, height):
-            if neighbor in unvisited:
-                neighbor_x, neighbor_y = neighbor
-                possible_current.add(neighbor)
-                tentative_distances[neighbor] = min(
-                    tentative_distances[neighbor],
-                    tentative_distances[current] + risk[neighbor_x, neighbor_y]
-                )
-        unvisited.remove(current)
-    return int(tentative_distances[end])
+        visited.add(current)
+        for neighbor in neighbors(current, width, height) - visited:
+            neighbor_x, neighbor_y = neighbor
+            possible_current.add(neighbor)
+            tentative_distances[neighbor] = min(
+                tentative_distances[neighbor],
+                tentative_distances[current] + risk[neighbor_x, neighbor_y]
+            )
+    return cast(int, tentative_distances[end])
 
 
-def rotate_risk(
-    risk: np_typing.NDArray[np.int64],
-    amount: int,
-) -> np_typing.NDArray[np.int64]:
+def rotate_risk(risk: Grid, amount: int) -> Grid:
     return (((risk + amount - 1) % 9) + 1)
 
 
@@ -70,19 +65,19 @@ def main() -> None:
     with open(f'{SCRIPT_DIR}/input.txt', 'r') as f:
         raw_risk = f.read()
     risk = parse_risk(raw_risk)
+    width, height = risk.shape
 
-    answer_1 = dijkstra(risk, (0, 0), (99, 99))
+    answer_1 = dijkstra(risk, (0, 0), (width-1, height-1))
     assert answer_1 == 604
     print(answer_1)
 
-    width, height = risk.shape
     bigger_risk = np.zeros((height * 5, width * 5), int)
     for i, j in product(range(5), range(5)):
         bigger_risk[i*width:(i+1)*width, j*height:(j+1)*height] = (
             rotate_risk(risk, i + j)
         )
 
-    answer_2 = dijkstra(bigger_risk, (0, 0), (499, 499))
+    answer_2 = dijkstra(bigger_risk, (0, 0), (width*5 - 1, height*5 - 1))
     assert answer_2 == 2907
     print(answer_2)
 
